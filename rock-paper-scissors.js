e73befe1de2025238395d81a8fa31fad68416298
eh-th <ehret@ikmail.com>
2022-05-29 10:36:18 +0200

    Create a first version of the game We can play five round and get the players win counter in the console at the end.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,35 @@
-console.log("hello")
+const outputs = ["rock", "paper", "scissors"]
+let computerSelection
+let playerScore = 0
+let computerScore = 0
+
+function computerPlay(choices) {
+    computerSelection = choices[Math.floor(Math.random() * choices.length)]
+    return computerSelection
+}
+
+function playerPlay() {
+    let playerSelection = prompt("Rock, Paper or Scissors?",'')
+    playerSelection = playerSelection.toLowerCase()
+    return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'paper' && computerSelection == 'rock')) {
+    playerScore++;
+    return "You win!"; 
+    } else {
+        return "You lose!";
+    }
+}   
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerPlay(), computerPlay(outputs))};
+    return `You won ${playerScore} rounds`;
+}
+
+
+console.log(game())
